dda16094f44f4a062b2821dfac973560d4a15aa7
Gonzalo Castiglione <gcastigl@gmail.com>
2012-03-29 18:13:00 -0300

    FindS Test was not compiling

--- Aprendizaje Automatico/src/FindSTest.java
+++ Aprendizaje Automatico/src/FindSTest.java
@@ -3,14 +3,14 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import finds.FindS;
-import finds.Hypotesis;
-import finds.implementation.LikesSportHypotesis;
+import finds.Hypothesis;
+import finds.implementation.LikesSportHypothesis;
 
 public class FindSTest {
 
 	public static void main(String[] args) {
-		FindS finds = new FindS(new LikesSportHypotesis());
-		Map<Hypotesis, Boolean> training = getTrainingExamples();
+		FindS finds = new FindS(new LikesSportHypothesis());
+		Map<Hypothesis, Boolean> training = getTrainingExamples();
 
 		System.out.println("About to train with: " + training);
 		// train
@@ -20,26 +20,26 @@ public class FindSTest {
 		testPositiveExamplesWithAlgorithm(finds, training);
 	}
 
-	private static Map<Hypotesis, Boolean> getTrainingExamples() {
+	private static Map<Hypothesis, Boolean> getTrainingExamples() {
 		String[] values;
-		Map<Hypotesis, Boolean> training = new HashMap<Hypotesis, Boolean>();
+		Map<Hypothesis, Boolean> training = new HashMap<Hypothesis, Boolean>();
 		// first set
 		values = new String[] {"soleado", "calido", "normal", "fuerte", "calida", "igual"};
-		training.put(new LikesSportHypotesis(values), true);
+		training.put(new LikesSportHypothesis(values), true);
 		// second set
 		values = new String[] {"soleado", "calido", "alta", "fuerte", "calida", "igual"};
-		training.put(new LikesSportHypotesis(values), true);
+		training.put(new LikesSportHypothesis(values), true);
 		// third set
 		values = new String[] {"nublado", "fria", "alta", "fuerte", "calida", "cambiante"};
-		training.put(new LikesSportHypotesis(values), false);
+		training.put(new LikesSportHypothesis(values), false);
 		// forth set
 		values = new String[] {"soleado", "calido", "alta", "fuerte", "fria", "cambiante"};
-		training.put(new LikesSportHypotesis(values), true);
+		training.put(new LikesSportHypothesis(values), true);
 		return training;
 	}
 	
-	private static void testPositiveExamplesWithAlgorithm(FindS algorithm, Map<Hypotesis, Boolean> training) {
-		for (Entry<Hypotesis, Boolean> entry: training.entrySet()) {
+	private static void testPositiveExamplesWithAlgorithm(FindS algorithm, Map<Hypothesis, Boolean> training) {
+		for (Entry<Hypothesis, Boolean> entry: training.entrySet()) {
 			System.out.println(entry.getValue() + " ==> " + algorithm.validates(entry.getKey()));
 		}
 	}
