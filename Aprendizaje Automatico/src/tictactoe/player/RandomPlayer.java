41f2a72e2ea59befc2ca69ddf117b079bff5975c
Gonzalo Castiglione <gcastigl@gmail.com>
2012-03-30 00:44:36 -0300

    cambio implementacion de RandomPlayer

--- Aprendizaje Automatico/src/tictactoe/player/RandomPlayer.java
+++ Aprendizaje Automatico/src/tictactoe/player/RandomPlayer.java
@@ -1,27 +1,35 @@
 package tictactoe.player;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import tictactoe.Game;
 import tictactoe.Mark;
+import tictactoe.ai.Position;
 
 public class RandomPlayer extends BasicPlayer {
 
 	public RandomPlayer(Mark mark) {
 		super(mark);
 	}
-	
+
 	@Override
 	public void makeMove(Game game) {
-		boolean moved = false;
-		while (!moved) {
-			int row = random.nextInt(3);
-			int col = random.nextInt(3);
-			if (!game.isSet(row, col)) {
-				game.put(mark, row, col);
-				moved = true;
+		List<Position> movements = new LinkedList<Position>();
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (!game.getBoard().isSet(i, j)) {
+					movements.add(new Position(i, j));
+				}
 			}
 		}
+		if (movements.isEmpty()) {
+			throw new IllegalStateException("No movements available!");
+		}
+		Position mov = movements.get(random.nextInt(movements.size()));
+		game.put(mark, mov.row, mov.column);
 	}
-	
+
 	@Override
 	public String getName() {
 		return "Random Player";
